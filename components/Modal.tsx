6947aeb0150a64e5299124e1ada46a0e5f098fe4
aramide ramadan <aramramadan7@gmail.com>
2025-06-05 12:39:31 +0100

    adjusted modal card

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -6,7 +6,7 @@ export default function Modal({ isOpen, onClose, children }) {
 
   return (
     <div className="fixed inset-0 bg-[rgba(0,0,0,0.5)] flex items-center justify-center z-50 h-screen">
-      <div className="bg-white p-6 rounded-md shadow-md max-w-md w-full relative">
+      <div className="bg-white p-4 rounded-md shadow-md max-w-md w-full mx-3 relative">
         <CircleX
           className="absolute w-8 h-8 top-6 right-6 fill-purple-600 text-white cursor-pointer"
           onClick={onClose}
