381ba537768143685d87bf94c69360c55dceb630
aramide ramadan <aramramadan7@gmail.com>
2025-07-03 12:24:28 +0100

    added live chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,21 @@ export default function RootLayout({
           }}
         />
         <AppProvider>{children}</AppProvider>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/68665a00a81b1119105e49a4/1iv7sv6fg';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+      })();
+    `,
+          }}
+        />
       </body>
     </html>
   );
